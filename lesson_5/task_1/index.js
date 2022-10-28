def1dbe5ad3eca2257ae5b369793bd0197d71a36
YaroslavINK <liviofanel@ukr.net>
2022-10-28 19:22:03 +0300

    some commits

---
+++ lesson_5/task_1/index.js
@@ -0,0 +1,123 @@
+// function getSum(a, b) {
+//   console.log(a + b);
+// }
+
+// getSum(2, 3);
+//  *****************************************************
+
+// function sayHi(name) {
+//   const greeting = 'Hi,  ' + name;
+//   console.log(greeting);
+// }
+// sayHi('John');
+// *************************************************************
+
+// function getEvenOdd(number) {
+//   if (number % 2 === 0) {
+//     return 'even';
+//   } else {
+//     return 'odd';
+//   }
+// }
+
+// getEvenOdd(17);
+// ******************************************
+
+// function getSquare(num) {
+//   return num * num;
+// }
+
+// const getSquare = (num) => num * num;
+
+// const result = getSquare(5);
+// console.log(result);
+// ***************************************************
+
+// function sum(firstNum, secondNum) {
+//   return firstNum + secondNum;
+// }
+
+// // examples
+// console.log(sum(11, 11)); // ==> 22
+// console.log(sum(-2, 10)); // ==> 8
+// ******************************************************
+
+// function sayHi(name) {
+//   console.log('Hi, ' + name);
+// }
+
+// sayHi('Lola');
+// *************************************************************
+
+// function getEvenOdd(number) {
+//   if (number % 2 === 0) {
+//     return 'even';
+//   } else {
+//     return 'odd';
+//   }
+// }
+// console.log(getEvenOdd(11));
+// *****************************************************************
+// const square = (num) => num * num;
+
+// console.log(square(5));
+// ****************************************************************
+
+// function getSum(startInterval, endInterval) {
+//   let result = 0;
+//   for (let i = startInterval; i <= endInterval; i += 1) {
+//     if (i % 2 === 0) {
+//       result += i;
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(getSum(1, 10));
+// *********************************************************************
+
+//put your code here
+// function increaser(a, index) {
+//   if (a > index) {
+//     return a + index;
+//   } else {
+//     return a;
+//   }
+// }
+
+// console.log(increaser(4, 2));
+// ****************************************************************************
+
+// function sum(from, to) {
+//   let result = 0;
+//   for (let i = from; i <= to; i += 1) {
+//     result += i;
+//   }
+//   return result;
+// }
+// // // examples
+// // console.log(sum(5, 10)); // ===> 45
+// // console.log(sum(11, 11)); // ===> 11
+
+// function compareSums(firstFrom, firstTo, secondFrom, secondTo) {
+//   const endResult = sum(firstFrom, firstTo) > sum(secondFrom, secondTo);
+//   return endResult;
+// }
+
+// console.log(compareSums(5, 10, 5, 10)); // ===> false
+// console.log(compareSums(5, 15, 3, 5)); // ===> true
+// console.log(compareSums(-2, 5, 5, 9)); // ===> false
+// **********************************************************************************************//
+
+// function findDivCount(a, b, n) {
+//   let result = 0;
+//   for (let i = a; i <= b; i += 1) {
+//     if (i % n === 0) {
+//       result += 1;
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(findDivCount(1, 5, 2));
+// ***************************************************************************************************
---
+++ lesson_6/index.js
@@ -0,0 +1,23 @@
+// const numbers = [11, 22, 33];
+// const numberOfElements = numbers.length;
+
+// console.log(numberOfElements);
+
+// console.log('Element #0: ' + numbers[0]);
+// console.log('Element #1: ' + numbers[1]);
+// console.log('Element last: ' + numbers[numbers.length - 1]);
+
+// const numbers = [11, 22, 33, 55, 66];
+
+// function getArrayBound(arr) {
+//   return [arr.length, arr[0], arr[arr.length - 1]];
+// }
+
+// console.log(getArrayBound(numbers));
+// *****************************************************************************************
+
+let numbe = [11, 22, 33, 55, 66];
+
+for (let i = 0; i < numbe.lenght; i += 1) {
+  console.log(numbe[i]);
+}

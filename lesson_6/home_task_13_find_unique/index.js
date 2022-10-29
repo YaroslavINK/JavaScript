38835eb0b61d625471208c881da3ec7c9c274e43
YaroslavINK <liviofanel@ukr.net>
2022-10-29 17:04:35 +0300

    ---

---
+++ lesson_6/home_task_13_find_unique/index.js
@@ -0,0 +1,10 @@
+function uniqueCount(array) {
+  if (!Array.isArray(array)) {
+    return null;
+  }
+  const newArr = [];
+  for (let i = 0; i < array.length; i += 1) {}
+}
+
+console.log(uniqueCount([1, 4, 1, 8, 3, 4, 8, 8]));
+// res => 4;

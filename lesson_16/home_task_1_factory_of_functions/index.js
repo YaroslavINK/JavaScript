ef8b64a3747f02b289c73309371e18a37eacbcb7
YaroslavINK <liviofanel@ukr.net>
2022-11-18 22:06:48 +0200

    completed task

---
+++ lesson_16/home_task_1_factory_of_functions/index.js
@@ -0,0 +1,46 @@
+//export
+
+// 1 input number wich === return arr.length
+// 2 return arr => arr.element = function(return function[i]);
+// 3 if typeof !=== number => return null
+// 4 if argument === undefined => return [];
+
+const createArrayOfFunctions = num => {
+  // if (num && typeof num !== 'number') {
+  //   return null;
+  // }
+  if (typeof num !== 'number' && num !== undefined) {
+    return null;
+  }
+  let resultArr = [];
+
+  for (let index = 0; index < num; index += 1) {
+    resultArr[index] = function () {
+      return index;
+    };
+  }
+
+  return resultArr;
+};
+
+console.log(createArrayOfFunctions(10)[9]());
+console.log(createArrayOfFunctions());
+console.log(createArrayOfFunctions('Ann'));
+
+// const createArrayOfFunctions = num => {
+//   if (num && typeof num !== 'number') {
+//     return null;
+//   }
+
+//   const arr = [];
+//   for (let i = 0; i < num; i++) {
+//     arr[i] = function () {
+//       return i;
+//     };
+//   }
+//   return arr;
+// };
+
+// console.log(createArrayOfFunctions(10)[9]());
+// console.log(createArrayOfFunctions());
+// console.log(createArrayOfFunctions('text'));
